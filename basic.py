c75550505ea9e707b09cf35db6bad18721c80935
ShawalarefiN <shawalarefin132@gmail.com>
2026-05-08 00:09:57 +0600

    idk

--- basic.py
+++ basic.py
@@ -1,2 +1,10 @@
-greet = 'Hello world!'
-print(greet)
+line01 = "*********************"
+line02 = "*                   *"
+line03 = "*      Welcome      *"
+
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+# Cmments
